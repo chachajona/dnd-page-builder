5474658fe49f0cca8d126dea53599b54ace40a37
Dung Dang <58243954+chachajona@users.noreply.github.com>
2024-01-03 16:44:32 +0700

    base setup

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Logo from "@/components/Logo";
+import ThemeSwitcher from "@/components/ThemeSwitcher";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col min-h-screen min-w-full bg-background max-h-screen">
+      <nav className="flex justify-between items-center border-b border-border h-[60px] px-4 py-2">
+        <Logo />
+        <ThemeSwitcher />
+      </nav>
+    </div>
+  );
+}
